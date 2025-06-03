b57c6d42513166b4d30c4a7a93f0d7eea30b45a8
TheDarkestCat90 <dombovschimarius@gmail.com>
2025-06-03 23:35:58 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function mobileMenu() {
+    var x = document.getElementById("navbar");
+
+    if (x.className === "") {
+        x.className = "mobile";
+    } else {
+        x.className = "";
+    }
+}
+
